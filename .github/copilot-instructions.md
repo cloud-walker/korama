2a625085ca231fa5926ff8bad1d2b9d4fef99c83
Luca Barone <baro.luc@gmail.com>
2025-05-29 20:32:38 +0200

    wip

---
+++ .github/copilot-instructions.md
@@ -0,0 +1,1 @@
+Please use pnpm over npm or yarn.
---
+++ packages/korama-react/src/main.test.ts
@@ -0,0 +1,19 @@
+import { expect, expectTypeOf, test } from "vitest";
+
+import * as API from "./main";
+
+test("API is respected", () => {
+  const APIKeys = Object.keys(API);
+
+  expect(APIKeys).toMatchInlineSnapshot(`
+    [
+      "Box",
+    ]
+  `);
+});
+
+test("Box component is created", () => {
+  expectTypeOf<API.BoxProps<"div">>().toEqualTypeOf<
+    React.ComponentPropsWithRef<"div">
+  >();
+});
