df8c75dc7898eca388cd4b44a1231c65dea0fd4c
Luca Barone <baro.luc@gmail.com>
2025-08-11 10:42:04 +0200

    chore: update react and utils packages
    - Updated `react/package.json` to add `@korama/utils` as a dependency and modified the lint script to include `attw`.
    - Removed `raise.ts` from `react/src` as it is no longer needed.
    - Enhanced `utils/package.json` with `type: module`, added `@arethetypeswrong/cli` and `@vitest/coverage-v8` as devDependencies, and updated the lint script.
    - Created `main.test.ts` in `utils/src` to ensure the contract is respected.
    - Added tests for the `raise` function in `raise.test.ts` to validate error handling.
    - Reintroduced `raise.ts` in `utils/src` with improved documentation.
    - Updated `utils/tsconfig.json` to include additional compiler options.
    - Added `vitest.config.ts` for configuring Vitest with coverage thresholds.
    - Updated `pnpm-lock.yaml` to reflect changes in dependencies and versions.

--- packages/utils/src/raise.ts
+++ packages/utils/src/raise.ts
@@ -1,3 +1,9 @@
+/**
+ * Functional way to throw an error, it's useful to be able
+ * to throw errors inside expressions.
+ *
+ * @param error The error to raise. It can be anything, if it's a string, it will be converted to an Error object.
+ */
 export function raise(error: unknown): never {
 	throw typeof error === "string" ? new Error(error) : error
 }
