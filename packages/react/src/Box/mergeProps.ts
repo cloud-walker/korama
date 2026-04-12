a595f2792113a1d1c51ddead5207c4eea1ad2bab
Luca Barone <baro.luc@gmail.com>
2026-04-12 12:55:27 +0200

    feat: integrate Valibot as the validation library and update mergeProps for enhanced type safety

--- packages/react/src/Box/mergeProps.ts
+++ packages/react/src/Box/mergeProps.ts
@@ -1,33 +1,31 @@
+import * as v from "valibot"
+
+const UnknownFunction = v.function()
+const UnknownRecord = v.record(v.string(), v.unknown())
+type UnknownRecord = v.InferOutput<typeof UnknownRecord>
+
 export function mergeProps(
-	base: Record<string, unknown>,
-	overrides: Record<string, unknown>,
-) {
+	base: UnknownRecord,
+	overrides: UnknownRecord,
+): UnknownRecord {
 	const props = {...base}
 
 	for (const [overrideKey, overrideValue] of Object.entries(overrides)) {
-		if (typeof overrideValue === "function" && overrideKey.startsWith("on")) {
+		if (v.is(UnknownFunction, overrideValue) && overrideKey.startsWith("on")) {
 			const baseValue = props[overrideKey]
-			if (typeof baseValue === "function") {
-				props[overrideKey] = (...args: unknown[]) => {
-					const result = overrideValue(...args)
-					baseValue(...args)
-					return result
-				}
+			if (v.is(UnknownFunction, baseValue)) {
+				props[overrideKey] = mergeEventHandlers(baseValue, overrideValue)
 				continue
 			}
 		}
 
 		if (overrideKey === "style") {
-			props.style = base.style
-				? {...base.style, ...(overrides.style as Record<string, unknown>)}
-				: overrides.style
+			props.style = mergeStyles(base.style, overrides.style)
 			continue
 		}
 
 		if (overrideKey === "className") {
-			props.className = base.className
-				? `${base.className} ${overrideValue}`
-				: overrideValue
+			props.className = mergeClassNames(base.className, overrides.className)
 			continue
 		}
 
@@ -35,4 +33,36 @@ export function mergeProps(
 	}
 
 	return props
+}
+
+function mergeEventHandlers(
+	rawBase: unknown,
+	rawOverride: unknown,
+): (...args: unknown[]) => unknown {
+	const override = v.parse(UnknownFunction, rawOverride)
+	if (!rawBase) {
+		return override
+	}
+	const base = v.parse(UnknownFunction, rawBase)
+	return (...args: unknown[]) => {
+		const result = override(...args)
+		base(...args)
+		return result
+	}
+}
+
+function mergeClassNames(rawBase: unknown, rawOverride: unknown): string {
+	const override = v.parse(v.string(), rawOverride)
+	if (!rawBase) {
+		return `${override}`
+	}
+	return `${v.parse(v.string(), rawBase)} ${override}`
+}
+
+function mergeStyles(rawBase: unknown, rawOverride: unknown): UnknownRecord {
+	const override = v.parse(UnknownRecord, rawOverride)
+	if (!rawBase) {
+		return override
+	}
+	return {...v.parse(UnknownRecord, rawBase), ...override}
 }
